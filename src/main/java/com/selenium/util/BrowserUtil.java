5a3192dbc2d62e953ac2ad75513ddd088625ec5d
channing.cheng <channing.cheng@dbappsecurity.com.cn>
2021-05-12 09:16:45 +0800

    dsadadas

--- src/main/java/com/selenium/util/BrowserUtil.java
+++ src/main/java/com/selenium/util/BrowserUtil.java
@@ -40,7 +40,7 @@ public class BrowserUtil {
      **/
     public static WebDriver openBrowser(String browserDriver) {
         if (AllConstant.CHROME.equals(browserDriver)) {
-            System.setProperty(AllConstant.CHROME_DRIVER, AllConstant.CHROME_DRIVER_ADDRESS);
+            System.setProperty(AllConstant.CHROME_DRIVER, "src/main/resources/driver/chromedriver");
             log.info(AllConstant.CHROME + "浏览器");
             driver = new ChromeDriver();
             return driver;
